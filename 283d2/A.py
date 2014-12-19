a3eae1192431a05b1ef19219c8366504ff394016
Luke <lrm96@cornell.edu>
2014-12-19 16:20:59 -0500

    small fix to A

--- 283d2/A.py
+++ 283d2/A.py
@@ -8,6 +8,7 @@ for x in range(n-1):
     origdiff = diff if origdiff > diff else origdiff
 
 mindiff = -10000
+
 for x in range(n-2):
     diff = min(arr[x] - arr[x+2], origdiff)
     mindiff = diff if diff > mindiff else mindiff
